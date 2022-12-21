9f5423dafa9df89228598a02d5fd9282d852cf72
03Simran <kaursp2003@gmail.com>
2022-12-21 16:06:53 +0530

    This is Simranpreet Kaur's task2 files

---
+++ prac2-pages/Simranpreet Kaur/login2.dart
@@ -0,0 +1,166 @@
+import 'package:flutter/material.dart';
+import 'login_page.dart';
+
+class Login2 extends StatefulWidget{
+  const Login2({Key? key}) : super(key:key);
+
+  @override
+  State<Login2> createState() => _Login2State();
+
+}
+class _Login2State extends State<Login2>
+{ var val2 = false;
+  String? str1 ;
+  var check = false;
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body:SafeArea(
+        child :Center(
+          child:Column(
+            children:  [
+              SizedBox(height: 55,),
+              //Login Verification
+              Text('Login Verification',
+              style : TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 30,
+                color: Color(0xFF06061C),
+              )),
+              SizedBox(height: 25,),
+              Center(
+                child: Padding(
+                  padding: EdgeInsets.fromLTRB(25,10,25,10),
+                  child: Text('To continue logging in we need you to complete one of the following:',
+                  textAlign: TextAlign.center,
+                  style : TextStyle(
+                    fontSize: 20,
+                  )),
+                ),
+              ),
+                SizedBox(height:25 ,),
+
+              RadioListTile(
+                title: Text("Verification code via email"),
+                activeColor: Color(0xFF06061C),
+                value: "email",
+                groupValue: str1,
+                onChanged: ((value) {
+                  setState(() {
+                    str1 = value.toString();
+                  });
+                }),
+              ),
+              RadioListTile(
+                title: Text("Verification code via SMS"),
+                activeColor: Color(0xFF06061C),
+                value: "SMS",
+                groupValue: str1,
+                onChanged: ((value) {
+                  setState(() {
+                    str1 = value.toString();
+                  });
+                }),
+              ),
+
+
+
+          SizedBox(height: 10,),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(60,0,25,10),
+            child: Text(
+                'We\'ll send a code to your mobile phone that can be entered to verify your identity.',
+                textAlign: TextAlign.left,
+                style: TextStyle(
+
+                  color: Colors.grey[700],
+                  fontSize: 15,
+                ),
+              ),
+
+          ),
+
+          SizedBox(height: 20),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0,0,210,0),
+            child: Container(
+              padding: EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                  color: Color(0xFF06061C),
+                  borderRadius: BorderRadius.circular(15)
+                  ),
+                height : 50,
+                width : 150,
+              child:  SizedBox(
+                width: 600.0,
+                height: 250.0,
+                child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: Color(0xFF06061C),
+                    ),
+                    child: const Text('Send Code',
+                      style: TextStyle(fontSize: 12,
+                      ),),
+                    onPressed: () {
+
+                    },
+                  ),
+              ),
+
+            ),
+          ),
+          SizedBox(height: 70),
+              Padding(
+                padding: const EdgeInsets.only(left:20.0),
+                child: Row(
+                  children: [
+                    Transform.scale(
+                        scale: 1.3,
+                        child: Checkbox( //only check box
+                            activeColor: Color(0xFF06061C),
+                            value: val2, //unchecked
+                            onChanged: (bool? value){
+                              //value returned when checkbox is clicked
+                              setState(() {
+                                val2 = value!;
+                              });
+                            }
+                        )
+                    ),
+                    Text(
+                        "   Remember this device",
+                        style: TextStyle(fontSize: 15.0,color:Color(0xFF06061C),fontWeight: FontWeight.bold, ),
+
+                    ),],),),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(50,0,25,10),
+                      child: Text(
+                        'Don\'t select this option if you are using a public computer.',
+                        textAlign: TextAlign.left,
+                        style: TextStyle(
+
+                          color: Colors.grey[700],
+                          fontSize: 15,
+                        ),
+                      ),
+                    ),
+              SizedBox(height: 80,),
+              TextButton(
+                style: TextButton.styleFrom(
+                  foregroundColor:  Colors.white,
+                ),
+                child: const Text('Return to Login',
+                  style: TextStyle(fontSize: 15,color: Colors.deepPurpleAccent,
+                  ),),
+                onPressed: () {
+                  Navigator. of(context). push(MaterialPageRoute(builder: (context)=>LoginPage()));
+                },
+              ),
+          ],
+        ),
+      ),),);
+
+
+  }
+}
---
+++ prac2-pages/Simranpreet Kaur/login_page.dart
@@ -0,0 +1,181 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'login2.dart' ;
+
+class LoginPage extends StatefulWidget{
+  const LoginPage({Key? key}) : super(key:key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+
+}
+class _LoginPageState extends State<LoginPage> {
+  bool check1 = true;
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child : Center(
+            child: Column(
+              children: [
+            SizedBox(height:55),
+             //Login
+             Text(
+               'Login',
+             style: TextStyle(
+               fontWeight: FontWeight.bold,
+               fontSize: 30,
+               color: Color(0xFF06061C),
+             ),
+             ),
+            SizedBox(height: 25),
+        //Create an account : Signup
+            Text('Create an account',
+              style:TextStyle(
+                color: Colors.deepPurpleAccent,
+                fontWeight: FontWeight.normal,
+                fontSize: 20,
+              ),
+            ),
+            SizedBox(height: 30,),
+            Container(
+              child: Column(
+                children: [
+
+                  Text('Username',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Color(0xFF06061C),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height:7),
+        //username textfield
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(color: Colors.grey),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Padding(
+                     padding: const EdgeInsets.only(left: 20.0),
+                      child :
+                      TextField(
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'Enter your username',
+                      contentPadding: EdgeInsets.symmetric(vertical: 20),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 30,),
+                Container(
+                  child: Text('Password',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Color(0xFF06061C),
+                    ),
+                  ),
+                ),
+            SizedBox(height: 7,),
+        //Password textfeild
+            Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(color: Colors.grey),
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 20.0),
+                      child: TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: 'Enter your password ',
+                          contentPadding: EdgeInsets.symmetric(vertical: 20),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+            SizedBox(height: 30),
+        //Login Button
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Container(
+                  decoration: BoxDecoration(
+                    color:Color(0xFF06061C),
+                    border: Border.all(color:Color(0xFF06061C), ),
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  height: 50,
+
+                  padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+                  child: SizedBox(
+                    width: 600.0,
+                    height: 250.0,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Color(0xFF06061C),
+                      ),
+                      child: const Text('Log In',
+                      style: TextStyle(fontSize: 20,
+                      ),),
+                      onPressed: () {
+                        Navigator. of(context). push(MaterialPageRoute(builder: (context)=>Login2()));
+                      },
+                    ),
+                  )
+              ),
+            ),
+            SizedBox(height: 20,),
+        //CheckBox : keep me logged in
+                Padding(
+                  padding: const EdgeInsets.only(left:20.0),
+                  child: Row(
+                    children: [
+                      Transform.scale(
+                        scale: 1.3,
+                        child: Checkbox( //only check box
+                          activeColor: Color(0xFF06061C),
+                            value: check1, //unchecked
+                            onChanged: (bool? value){
+                              //value returned when checkbox is clicked
+                              setState(() {
+                                check1 = value!;
+                              });
+                            }
+                        )
+                      ),
+                      Text(
+                        "Keep me logged in",
+                        style: TextStyle(fontSize: 18.0,)
+                      ),
+                    ],
+                  ),
+                ),
+            SizedBox(height: 20),
+        //Forgot Password?
+                Text('Forgot Password?',
+                  style:TextStyle(
+                    color: Colors.deepPurpleAccent,
+                    fontWeight: FontWeight.normal,
+                    fontSize: 20,
+                  ),
+                ),
+      ],)
+    ),),);
+  }
+}
