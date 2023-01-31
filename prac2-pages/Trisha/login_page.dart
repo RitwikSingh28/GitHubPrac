34beda88f9701d66dec38e624d77fc9bb2c3479a
TRISHAHALDER <98026599+TRISHAHALDER@users.noreply.github.com>
2023-01-31 20:31:07 +0530

    Update login_page.dart

--- prac2-pages/Trisha/login_page.dart
+++ prac2-pages/Trisha/login_page.dart
@@ -1,11 +1,10 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:myflutter/verification.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
   @override
   State<LoginPage> createState() => _MyLoginState();
-  
 }
 
 class _MyLoginState extends State<LoginPage> {
@@ -13,7 +12,7 @@ class _MyLoginState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 183, 173, 197),
+      backgroundColor: Color.fromARGB(255, 254, 254, 255),
       body: SafeArea(
         child: Center(
           child: Column(
@@ -21,13 +20,13 @@ class _MyLoginState extends State<LoginPage> {
               Icon(
                 Icons.android,
                 size: 150,
-                color: Color.fromARGB(255, 172, 123, 120),
+                color: Color.fromARGB(255, 10, 10, 10),
               ),
               SizedBox(height: 20),
               Text(
-                'Hello!',
+                'Login',
                 style: TextStyle(
-                  color: Color.fromARGB(73, 211, 4, 4),
+                  color: Colors.black,
                   fontWeight: FontWeight.bold,
                   fontSize: 35,
                 ),
@@ -46,8 +45,8 @@ class _MyLoginState extends State<LoginPage> {
                 padding: const EdgeInsets.symmetric(horizontal: 25.0),
                 child: Container(
                   decoration: BoxDecoration(
-                    color: Color.fromARGB(255, 143, 211, 178),
-                    border: Border.all(color: Colors.transparent),
+                    color: Color.fromARGB(255, 247, 249, 249),
+                    border: Border.all(color: Color.fromARGB(255, 214, 205, 205)),
                     borderRadius: BorderRadius.circular(15),
                   ),
                   child: Padding(
@@ -66,8 +65,8 @@ class _MyLoginState extends State<LoginPage> {
                 padding: const EdgeInsets.symmetric(horizontal: 25.0),
                 child: Container(
                   decoration: BoxDecoration(
-                    color: Color.fromARGB(255, 143, 211, 178),
-                    border: Border.all(color: Colors.transparent),
+                    color: Color.fromARGB(255, 249, 250, 250),
+                    border: Border.all(color: Color.fromARGB(255, 197, 191, 191)),
                     borderRadius: BorderRadius.circular(15),
                   ),
                   child: Padding(
@@ -83,41 +82,47 @@ class _MyLoginState extends State<LoginPage> {
                 ),
               ),
               SizedBox(height: 20),
-              
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 25.0),
                 child: Container(
                   padding: EdgeInsets.all(20),
                   decoration: BoxDecoration(
-                      color: Color.fromARGB(255, 185, 136, 120),
-                      borderRadius: BorderRadius.circular(15)),
-                  child: Center(
+                    border: Border.all(color: Colors.grey),
+                      color: Color.fromARGB(255, 6, 6, 6),
+                      borderRadius: BorderRadius.circular(15)
+                      ),
+                      height: 60,
+                      width:300,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        primary: const Color(0xFF06061C)),
                     child: Text(
-                      'Sign In',
+                      'Log In',
                       style: TextStyle(
-                          color: Color.fromARGB(255, 99, 29, 8),
+                          color: Color.fromARGB(255, 255, 253, 253),
                           fontWeight: FontWeight.bold,
                           fontSize: 15),
                     ),
+                    onPressed: () {
+                      Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => const LoginVerification()));
+                    },
                   ),
                 ),
               ),
               SizedBox(height: 10),
               Row(
-               children: [ 
-                SizedBox(width: 15),
-                Checkbox(
-                value: this.valuefirst,
-                onChanged: (value) {
-                  setState(() {
-                    this.valuefirst = true;
-                  });
-                },
-              ),
-              SizedBox(width: 5),
-                
-               
-        
+                children: [
+                  SizedBox(width: 15),
+                  Checkbox(
+                    value: valuefirst,
+                    onChanged: (value) {
+                      setState(() {
+                        valuefirst = value!;
+                      });
+                    },
+                  ),
+                  SizedBox(width: 1),
                   Text('Not a member?'),
                   Text(
                     'Register Now',
