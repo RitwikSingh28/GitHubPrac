6ccc7c4ff603cf4ad9f9e3e60d911659a011f2e2
TRISHAHALDER <98026599+TRISHAHALDER@users.noreply.github.com>
2023-01-31 20:34:37 +0530

    Update verification.dart

--- prac2-pages/Trisha/verification.dart
+++ prac2-pages/Trisha/verification.dart
@@ -1,6 +1,6 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'dart:ui';
+
 class LoginVerification extends StatefulWidget {
   const LoginVerification({Key? key}) : super(key: key);
   @override
@@ -13,7 +13,7 @@ class _VerifyState extends State<LoginVerification> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blueGrey,
+      backgroundColor: Colors.white,
       body: SafeArea(
         child: Center(
           child: Column(
@@ -24,7 +24,7 @@ class _VerifyState extends State<LoginVerification> {
               Text(
                 'Login Verification',
                 style: TextStyle(
-                  color: Colors.deepPurple,
+                  color: Colors.black,
                   fontWeight: FontWeight.bold,
                   fontSize: 30,
                 ),
@@ -35,61 +35,63 @@ class _VerifyState extends State<LoginVerification> {
               Text(
                 'To continue complete following steps',
                 style: TextStyle(
-                  color: Color.fromARGB(255, 75, 10, 49),
+                  color: Colors.black,
                   fontWeight: FontWeight.normal,
                   fontSize: 15,
                 ),
               ),
-              SizedBox(height: 100,),
+              SizedBox(
+                height: 100,
+              ),
               Column(
                 children: [
                   RadioListTile(
-                      title: Text("Verification code via email"),
-                      value: "email",
-                      groupValue: verify,
-                      onChanged: ((value) {
-                        setState(() {
-                          verify = value.toString();
-                        });
-                      }),
-                      ),
-                       RadioListTile(
-                      title: Text("Verification code via SMS"),
-                      value: "SMS",
-                      groupValue: verify,
-                      onChanged: ((value) {
-                        setState(() {
-                          verify = value.toString();
-                        });
-                      }),
-                      ),
+                    title: Text("Verification code via email"),
+                    value: "email",
+                    groupValue: verify,
+                    onChanged: ((value) {
+                      setState(() {
+                        verify = value.toString();
+                      });
+                    }),
+                  ),
+                  RadioListTile(
+                    title: Text("Verification code via SMS"),
+                    value: "SMS",
+                    groupValue: verify,
+                    onChanged: ((value) {
+                      setState(() {
+                        verify = value.toString();
+                      });
+                    }),
+                  ),
                 ],
               ),
-              SizedBox(height: 10,),
+              SizedBox(
+                height: 10,
+              ),
               Text(
                 '  We"ll send a otp to your mobile phone verify your identity',
-              textAlign: TextAlign.center,
+                textAlign: TextAlign.center,
                 style: TextStyle(
-                  
                   color: Color.fromARGB(83, 4, 1, 8),
                   fontWeight: FontWeight.bold,
                   fontSize: 15,
                 ),
               ),
               SizedBox(height: 20),
-              
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 25.0),
                 child: Container(
                   padding: EdgeInsets.all(20),
                   decoration: BoxDecoration(
-                      color: Color.fromARGB(255, 91, 81, 78),
+                      color: Color.fromARGB(255, 9, 9, 9),
                       borderRadius: BorderRadius.circular(15)),
                   child: Center(
                     child: Text(
                       'Send Code',
                       style: TextStyle(
-                          color: Color.fromARGB(255, 26, 18, 16),
+                          color: Color.fromARGB(255, 247, 247, 247),
                           fontWeight: FontWeight.bold,
                           fontSize: 15),
                     ),
@@ -98,19 +100,18 @@ class _VerifyState extends State<LoginVerification> {
               ),
               SizedBox(height: 10),
               Row(
-               children: [ 
-                SizedBox(width: 15),
-                Checkbox(
-                value: this.valuefirst,
-
-                onChanged: (value) {
-                  setState(() {
-                    this.valuefirst = true;
-                  });
-                },
-              ),
-              SizedBox(width: 5),
-                  
+                children: [
+                  SizedBox(width: 15),
+                  Checkbox(
+                    value: valuefirst,
+                    onChanged: (bool? value) {
+                      setState(() {
+                        valuefirst =
+                            value!; //return the opposite of current bool value on tapping
+                      });
+                    },
+                  ),
+                  SizedBox(width: 5),
                   Text(
                     'Remember this device',
                     style: TextStyle(
@@ -126,3 +127,4 @@ class _VerifyState extends State<LoginVerification> {
     );
   }
 }
+
